8556b6288c74de9689b74d73ed1e99ffcb10d709
Ruchir <ruchir@pdx.edu>
2023-04-05 15:00:11 -0700

    Initial Commit

---
+++ Fetch-TakeHome/Fetch_TakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_TakeHomeApp.swift
+//  Fetch-TakeHome
+//
+//  Created by Ruchir on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_TakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
